7df73fc8f66cf4030ee4cd7c6eb676e27ca9e376
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-02-02 04:01:34 +0700

    Complete game!

--- web/js/main.js
+++ web/js/main.js
@@ -8,14 +8,30 @@ var except_bet_type = null;
 var memes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var used_memes = [];
 
-var intro = $("#intro");
-var fr_main = $("#fr_main");
-var btn_start = $("#btn_start");
+var current_result = 0;
+
+var is_opened = false;
+var is_shacking = false;
+var is_dragging = false;
+var started = false;
+
+var gen_rs;
+var open_rs;
+
+var bet_time = 20;
+var open_time = 10;
+
+var user_choice;
+
+const intro = $("#intro");
+const fr_main = $("#fr_main");
+const btn_start = $("#btn_start");
+const header = $("#header_txt");
+const cover = $('#cover');
+const countdown = $('#countdown');
 
 $(document).ready(function () {
-    // fr_main.hide();
-    formatTotalMoney();
-    checkBetLv();
+    fr_main.hide();
 
     //Xác nhận điều khoản
     btn_start.on({
@@ -24,6 +40,11 @@ $(document).ready(function () {
                 $(this).click(() => {
                     intro.hide(500);
                     fr_main.show(500);
+                    setTimeout(()=>{
+                        randCube();
+                        runGame();
+                        formatTotalMoney();
+                    });
                 });
             } else {
                 var randomTop = Math.floor(Math.random() * (intro.outerHeight() - btn_start.outerHeight() * 2)) + btn_start.outerHeight() / 2;
@@ -38,12 +59,11 @@ $(document).ready(function () {
     //End confirm
 
     //Hoạt ảnh kéo thả bát úp
-    var cover = $('#cover');
-    f_offsetX = cover.offset().left;
-    f_offsetY = cover.offset().top;
+    f_offsetX = 172;
+    f_offsetY = 29;
 
     cover.mousedown(function (e) {
-        isDragging = true;
+        is_dragging = true;
 
         offsetX = e.pageX;
         offsetY = e.pageY;
@@ -54,11 +74,11 @@ $(document).ready(function () {
     });
 
     $(document).mouseup(function () {
-        isDragging = false;
+        is_dragging = false;
     });
 
     cover.mousemove(function (e) {
-        if (isDragging) {
+        if (is_dragging && !is_opened && !is_shacking) {
             var x = e.pageX - offsetX;
             var y = e.pageY - offsetY;
 
@@ -68,9 +88,15 @@ $(document).ready(function () {
             x = Math.min(Math.max(x, 0 - cover.width() / 2), maxX);
             y = Math.min(Math.max(y, 0 - cover.height() / 2), maxY);
 
-            cover.css({ left: x, top: y });
+            if (x <= 112 || x >= 280 || y <= -145 || y >= 238 || (y >= 210 && x <= -64) || (x >= 149 && y >= 210) || (y <= -64 && x <= 182) || (y >= 175 && x <= 235)) {
+                clearTimeout(gen_rs);
+                result();
+            } else {
+                cover.css({ left: x, top: y });
+            }
         }
     });
+
     //End hoạt ảnh
 
     //Event click mức cược
@@ -93,14 +119,56 @@ $(document).ready(function () {
         });
     });
     //End event
-
-    randCube();
 });
+
+function shakePlate() {
+
+    if (!is_shacking) {
+        is_shacking = true;
+
+
+        started = true;
+        clearTimeout(gen_rs);
+        if (is_opened) {
+            is_opened = false;
+        }
+        if (cover.hasClass("open")) {
+            cover.removeClass("open");
+        };
+        cover.css({ left: f_offsetX, top: f_offsetY });
+        setTimeout(function () {
+            // shakePlate();
+
+            $("#fr_animation").addClass("shake");
+            header.html("Xóc-ing...");
+            randCube();
+            setTimeout(function () {
+                is_shacking = false;
+                $("#fr_animation").removeClass("shake");
+                header.html("bắt đầu đặt cược");           
+                if ($("#bet_b").hasClass("bet_inactive")) {
+                    $("#bet_b").removeClass("bet_inactive");
+                }
+                if ($("#bet_s").hasClass("bet_inactive")) {
+                    $("#bet_s").removeClass("bet_inactive");
+                }
+            }, 2000);
+
+        }, 500);
+    }
+    //  else {
+    //     let note = $("<span class='note'>Đang xóc! Bấm ít thôi :))</span>");
+    //     $("#notify").append(note);
+    //     setTimeout(function () {
+    //         note.remove();
+    //     }, 1500);
+    // }
+}
 
 function onBet(el) {
     except_bet_type = $(el).attr('data-except-bet-type');
+    user_choice = $(el).attr('data-bet');
     $(`#bet_${except_bet_type}`).addClass("bet_inactive").off("click");
-    $(`#fr_${except_bet_type}`).addClass("bet_inactive");
     if (total_money >= 1000) {
         if (current_bet_lv > 500) {
             total_bet += total_money;
@@ -130,10 +198,9 @@ function activeBet() {
 
 }
 
-function reActiveBet() {
+function reActivateBet() {
     if (except_bet_type != null) {
         $(`#bet_${except_bet_type}`).removeClass("bet_inactive");
-        $(`#fr_${except_bet_type}`).removeClass("bet_inactive");
         total_bet = 0;
         formatTotalBet(except_bet_type, total_bet);
         formatTotalMoney();
@@ -160,7 +227,6 @@ function formatTotalBet(type, val) {
 
 //+500k
 function add500() {
-    reActiveBet();
     // Rand meme
     const rand_meme = memes.splice(Math.floor(Math.random() * memes.length), 1)[0];
     $("#content_meme").css('background-image', `url('/web/material/meme/meme${rand_meme}.png')`);
@@ -248,7 +314,8 @@ function rollADice() {
 }
 
 function randCube() {
-    for(let i = 1; i<=3;i++) {
+    current_result = 0;
+    for (let i = 1; i <= 3; i++) {
         let fr_cube = $(`#c${i}`);
         if (fr_cube.length > 0) {
             fr_cube.remove();
@@ -257,16 +324,103 @@ function randCube() {
         fr_cube = $(`<div class='cube' id='c${i}'></div>`);
         rollADice();
         let top = $(`<span class="ct"></span>`);
-        top.css('background-image',`url("/web/material/t${ct}.png")`);
+        top.css('background-image', `url("./web/material/t${ct}.png")`);
         let left = $(`<span class="cl"></span>`);
-        left.css('background-image',`url("/web/material/l${cl}.png")`);
+        left.css('background-image', `url("./web/material/l${cl}.png")`);
         let right = $(`<span class="cr"></span>`);
-        right.css('background-image',`url("/web/material/r${cr}.png")`);
+        right.css('background-image', `url("./web/material/r${cr}.png")`);
         fr_cube.append(top);
         fr_cube.append(left);
         fr_cube.append(right);
-        console.log(fr_cube);
         $("#cubes").append(fr_cube);
+        current_result += ct;
         ct = cl = cr = null;
     }
 }
+
+function result() {
+    let rs = current_result;
+    is_opened = true;
+    cover.addClass("open");
+    if (started) {
+        header.html(`${rs} điểm - ${(rs > 10) ? "Tài" : "Xỉu"}`);
+        addToResults(rs);
+    }
+
+    if(user_choice == (rs >10)?1:0) {
+        total_money += total_bet*2;
+        formatTotalMoney();
+    }
+}
+
+function runGame() {
+    checkBetLv();
+    reActivateBet();
+    shakePlate();
+    
+    $("#total_bet_b").html("");
+    $("#total_bet_s").html("");
+    if (countdown.hasClass("ending")) {
+        countdown.removeClass("ending");
+    }
+    countdown.html(bet_time);
+    setTimeout(()=>{
+        let rm_time = setInterval(() => {
+            if (bet_time <= 5) {
+                countdown.addClass("ending");
+            } else {
+                if (countdown.hasClass("ending")) {
+                    countdown.removeClass("ending");
+                }
+            }
+            bet_time -= 1;
+            countdown.html(bet_time);
+    
+            if (bet_time == 0) {
+                clearInterval(rm_time);
+                bet_time = 20;
+                
+                if (!$("#bet_b").hasClass("bet_inactive")) {
+                    $("#bet_b").addClass("bet_inactive");
+                }
+                if (!$("#bet_s").hasClass("bet_inactive")) {
+                    $("#bet_s").addClass("bet_inactive");
+                }
+                header.html("ngừng cược!");
+                setTimeout(function () {
+                    open();
+                }, 1000);
+            }
+        }, 1000)
+    }, 2500);
+}
+
+function open() {
+    header.html("mời mở");
+    countdown.html(open_time);
+    let op_time = setInterval(() => {
+        open_time -= 1;
+        countdown.html(open_time);
+
+        if (open_time < 0) {
+            clearInterval(op_time);
+            open_time = 10;
+            header.html("");
+            runGame();
+        }
+    }, 1000);
+    gen_rs =
+    setTimeout(function () {
+        result();
+    }, (open_time*1000 - 3000));
+}
+
+function addToResults(rs) {
+
+    if ($("#fr_result").children().length == 15) {
+        $("#fr_result").children().first().remove();
+    }
+
+    let new_rs = $(`<div class='dot_rs dot_${(rs > 10)?"b":"s"}'></div>`);
+    $("#fr_result").append(new_rs);
+  }
